ab6c36b45d8b4d1dd15c23465fd92097ec3edf11
Santhosh-Chinnasamy <csesanthosh15@gmail.com>
2019-12-13 15:24:25 +0530

    Finish Guessing Game :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
 	println!("Guess the Number");
 	let secrect_number = rand::thread_rng().gen_range(1, 100);
-	println!("The Secrect number is {}", secrect_number);
-	println!("Please input your guess");
-	let mut guess = String::new();
-	io::stdin()
-		.read_line(&mut guess)
-		.expect("failed to read line");
 
-	println!("You guessed {}", guess);
+	loop {
+		println!("Please input your guess 1 to 100");
+		let mut guess = String::new();
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("failed to read line");
+
+		// let guess: u32 = guess.trim().parse().expect("Please Enter a number");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
+		println!("You guessed {}", guess);
+		match guess.cmp(&secrect_number) {
+			Ordering::Less => println!("Too Small"),
+			Ordering::Greater => println!("Too High"),
+			Ordering::Equal => {
+				println!("You Won");
+				break;
+			}
+		}
+	}
 }
